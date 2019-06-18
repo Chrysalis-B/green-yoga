8023686fe969bc522464ee01e50b070ed89961c9
Heini Ahokas <ahokas.heini@gmail.com>
2019-06-19 01:32:14 +0200

    Add gatsby- background-image to hero image component

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,30 +1,46 @@
 import React from "react"
-import Img from "gatsby-image/withIEPolyfill"
-import { useStaticQuery } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 import "./hero.css"
+import useState from "react"
+
+import BackgroundImage from 'gatsby-background-image'
 
 
 export default () => {
     const data = useStaticQuery(
         graphql`
         query {
-          file(relativePath: { eq: "assets/hero-img-planting.jpg" }) {
-            childImageSharp {
-              fluid(maxWidth: 1800 maxHeight: 1000) {
-                ...GatsbyImageSharpFluid
-              }
-            }
-          }
+            image1: 
+                file(relativePath: { eq: "assets/hero-img-studio-one.jpg" }) {
+                    childImageSharp {
+                      fluid(maxWidth: 1800 maxHeight: 1000) {
+                        ...GatsbyImageSharpFluid
+                      }
+                    }
+                }
+            image2: 
+                file(relativePath: { eq: "assets/hero-img-planting.jpg" }) {
+                    childImageSharp {
+                      fluid(maxWidth: 1800 maxHeight: 1000) {
+                        ...GatsbyImageSharpFluid
+                      }
+                    }
+                }
+            image3: 
+                file(relativePath: { eq: "assets/hero-img-studio-two.jpg" }) {
+                    childImageSharp {
+                      fluid(maxWidth: 1800 maxHeight: 1000) {
+                        ...GatsbyImageSharpFluid
+                      }
+                    }
+                }
         }
       `
     );
+    const images = [data.image1.childImageSharp.fluid, data.image2.childImageSharp.fluid, data.image3.childImageSharp.fluid];
     return (
         <div className="hero-section__image-container">
-            <Img fluid={data.file.childImageSharp.fluid}
-        objectFit="cover"
-        objectPosition="50% 50%"
-        alt="" />
+            <BackgroundImage className="hero-section__background-image" fluid={images}/>
         </div>
-        
     )
 }
