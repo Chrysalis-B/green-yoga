a64fad04a1cebd5ee99dfe49471b7d2631acffff
Heini Ahokas <ahokas.heini@gmail.com>
2019-07-03 22:43:37 +0300

    Add link to timetable

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -10,6 +10,7 @@ export default ({data}) => {
         <Layout>
             <React.Fragment>
                 <h1 className="schedule__title">{title}</h1>
+                <a className="schedule__link-to-sheet" target="_blank" rel="noopener noreferrer" href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-hHMaWeuBxLfpUezOefzVNU9aiu1gDmgJGxb0KYsCQPL0-Xg6SHHhoKFmLOf_d1yo-wfZPlizC-Bo/pubhtml?gid=354688609&single=true">Link to schedule</a>
                 <iframe title="schedule-sheet" className="schedule__timetable-sheet" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-hHMaWeuBxLfpUezOefzVNU9aiu1gDmgJGxb0KYsCQPL0-Xg6SHHhoKFmLOf_d1yo-wfZPlizC-Bo/pubhtml?gid=354688609&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
                 <div className="schedule__classes-container" dangerouslySetInnerHTML={{ __html: content }}></div>
             </React.Fragment> 
