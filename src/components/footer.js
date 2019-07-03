d05b8da34ec1ca21cb6e2739639ab6c190716a25
Heini Ahokas <ahokas.heini@gmail.com>
2019-07-03 22:26:02 +0300

    Add footer data

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import Img from "gatsby-image/withIEPolyfill"
 import PhoneIcon from "../assets/icons/phone.svg"
 import SmsIcon from "../assets/icons/sms.svg"
 import { useStaticQuery, graphql } from "gatsby"
-
+import FooterData from "../data/footer.json"
 import "./footer.css"
 
 export default () => {
@@ -45,31 +45,40 @@ export default () => {
     <footer className="footer__wrapper">
         <address className="footer__container">
             <div className="footer__address-partition">
-                <p className="footer__address-item">Green Yoga</p>
-                <p className="footer__address-item">Srikanth Reddy Beeram</p>
+                <p className="footer__address-item">{FooterData.companyName}</p>
+                <p className="footer__address-item">{FooterData.name}</p>
                 <div><Link className="footer__address-item-link" to="/imprint">Imprint</Link></div> 
             </div>
             <div className="footer__address-partition">
-                <div><a className="footer__address-item-link" href="https://goo.gl/maps/87LAAiKgWsxpH5b46" target="_blank" rel="noopener noreferrer">Sredzkistr. 59</a></div>
-                <div><a className="footer__address-item-link" href="https://goo.gl/maps/87LAAiKgWsxpH5b46" target="_blank" rel="noopener noreferrer">10405 Berlin</a></div>
+                <div><a className="footer__address-item-link" href={FooterData.address.url} target="_blank" rel="noopener noreferrer">{FooterData.address.street}</a></div>
+                <div><a className="footer__address-item-link" href={FooterData.address.url} target="_blank" rel="noopener noreferrer">{FooterData.address.zip} {FooterData.address.city}</a></div>
             </div>
         </address>
         <div className="footer__container">
             <div className="footer__address-partition">
-                <div><img className="footer__address-icon-phone" src={PhoneIcon} alt="call"/> <img className="footer__address-icon-phone" src={SmsIcon} alt="text"/><a className="footer__address-item-link" href="tel:49-1771431355">+49 1771 431355</a></div>
-                <img className="footer__address-icon-phone" src={PhoneIcon} alt="call"/> <img className="footer__address-icon-phone" src={SmsIcon} alt="text"/><a className="footer__address-item-link" href="tel:49-15781974844">+49 1578 1974844</a>
+                <div>
+                    <img className="footer__address-icon-phone" src={PhoneIcon} alt="call"/> 
+                    <img className="footer__address-icon-phone" src={SmsIcon} alt="text"/>
+                    <a className="footer__address-item-link" href={FooterData.tel1.url}>{FooterData.tel1.number}</a>
+                </div>
+                <div>
+                    <img className="footer__address-icon-phone" src={PhoneIcon} alt="call"/> 
+                    <img className="footer__address-icon-phone" src={SmsIcon} alt="text"/>
+                    <a className="footer__address-item-link" href={FooterData.tel2.url}>{FooterData.tel2.number}</a>
+                </div>
+                
             </div>
         </div>
         <div className="footer__container">
             <div className="footer__address-partition">
-                <a href="https://www.facebook.com/greenyoga.org" target="_blank" rel="noopener noreferrer">
+                <a href={FooterData.socialMedia.facebook} target="_blank" rel="noopener noreferrer">
                     <Img style={styleIcon} fixed={data.facebook.childImageSharp.fixed} alt="Facebook" />
                 </a>
 
-                <a href="https://wa.me/00491771431355/" target="_blank" rel="noopener noreferrer">
+                <a href={FooterData.socialMedia.whatsapp} target="_blank" rel="noopener noreferrer">
                     <Img style={styleIcon} fixed={data.whatsapp.childImageSharp.fixed} alt="WhatsApp" />
                 </a>
-                <a href="https://www.instagram.com/greenyoga.co/" target="_blank" rel="noopener noreferrer">
+                <a href={FooterData.socialMedia.instagram} target="_blank" rel="noopener noreferrer">
                     <Img fixed={data.instagram.childImageSharp.fixed} alt="Instagram" />
                 </a>
             </div>
