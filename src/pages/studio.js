e8f318ea463fc0e333edabf67475bc92d6488894
Heini Ahokas <ahokas.heini@gmail.com>
2019-07-07 16:58:35 +0300

    Add content and styling to studio page

--- src/pages/studio.js
+++ src/pages/studio.js
@@ -3,38 +3,43 @@ import Layout from "../components/layout"
 import ContactData from "../data/contact.json"
 import { graphql } from 'gatsby'
 import Img from "gatsby-image/withIEPolyfill"
+import "./studio.css"
 
 
 
 export default ({data}) => {
     const images = data.allFile.edges;
     const imageStyles = {
-        margin: '0.75rem',
+        margin: '0.75rem 0',
         borderRadius: '0.3rem'
     };
     return (
     <Layout>
-        <div className="studio">
-                <div className="contact">
-                    <p>Green Yoga</p>
-                    <a href={ContactData.address.url} target="_blank" rel="noopener noreferrer"><p>{ContactData.address.street}</p></a>
-                    <a href={ContactData.address.url} target="_blank" rel="noopener noreferrer"><p>{ContactData.address.zip} {ContactData.address.city}</p></a>
-                    <a href={ContactData.tel1.url}><p>{ContactData.tel1.number}</p></a>
-                    <a href={ContactData.tel2.url}><p>{ContactData.tel2.number}</p></a>
-                </div>
+        <h2 className="studio__title">Where to find us</h2>
+        <div className="studio__container">
+            <div className="studio__address">
+                <p className="studio__address-item">Green Yoga</p>
+                <a className="studio__address-item" href={ContactData.address.url} target="_blank" rel="noopener noreferrer"><p>{ContactData.address.street}</p></a>
+                <a className="studio__address-item" href={ContactData.address.url} target="_blank" rel="noopener noreferrer"><p>{ContactData.address.zip} {ContactData.address.city}</p></a>
+                <a className="studio__address-item" href={ContactData.tel1.url}><p>{ContactData.tel1.number}</p></a>
+                <a className="studio__address-item" href={ContactData.tel2.url}><p>{ContactData.tel2.number}</p></a>
+            </div> 
+            <div className="studio__map">
+                <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2426.7484859330143!2d13.419246717295579!3d52.5379846596298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e0123a140b5%3A0x249d1cbdc20bfd80!2sSredzkistra%C3%9Fe+59%2C+10405+Berlin!5e0!3m2!1sen!2sde!4v1559236774966!5m2!1sen!2sde" width="600" height="450" frameBorder="0"></iframe>
+            </div>
+        </div>
+        <h2 className="studio__title">Our space</h2>
+        <div className="studio__container">
             {images.map( image => {
             return (
-            <Img fixed={image.node.childImageSharp.fixed}
+            <Img fluid={image.node.childImageSharp.fluid}
             key={image.node.id}
             style={imageStyles}
             objectFit="cover"
             objectPosition="50% 50%"
             alt="" />  
             )    
-        })}
-                {/* <div className="map">
-                    <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2426.7484859330143!2d13.419246717295579!3d52.5379846596298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e0123a140b5%3A0x249d1cbdc20bfd80!2sSredzkistra%C3%9Fe+59%2C+10405+Berlin!5e0!3m2!1sen!2sde!4v1559236774966!5m2!1sen!2sde" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-                </div> */}
+        })} 
         </div>
     </Layout>
 )}
@@ -46,8 +51,8 @@ query {
           node {
               id
               childImageSharp {
-              fixed(width: 250 height:300) {
-                ...GatsbyImageSharpFixed
+              fluid(maxWidth: 600 maxHeight:450) {
+                ...GatsbyImageSharpFluid
               }
             }
           }
