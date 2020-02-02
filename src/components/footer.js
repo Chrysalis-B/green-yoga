464ed6032575eb0f50ecb6bd0eee30fc7a5aaa73
Heini Ahokas <ahokas.heini@gmail.com>
2020-02-02 16:49:01 +0100

    Restyle footer to include x-berg info

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,85 +4,200 @@ import Img from "gatsby-image/withIEPolyfill"
 import PhoneIcon from "../assets/icons/phone.svg"
 import SmsIcon from "../assets/icons/sms.svg"
 import { useStaticQuery, graphql } from "gatsby"
-import ContactData from "../data/contact-p-berg.json"
+import ContactDataPBerg from "../data/contact-p-berg.json"
+import ContactDataXBerg from "../data/contact-x-berg.json"
 import NavigationData from "../data/navigation.json"
 import "./footer.css"
 
 export default () => {
     const data = useStaticQuery(
         graphql`
-        query {
-            facebook: 
-                file(relativePath: { eq: "assets/icons/facebook.png" }) {
+            query {
+                facebook: file(
+                    relativePath: { eq: "assets/icons/facebook.png" }
+                ) {
                     childImageSharp {
                         fixed(width: 40) {
-                          ...GatsbyImageSharpFixed
+                            ...GatsbyImageSharpFixed
                         }
                     }
                 }
-            whatsapp: 
-                file(relativePath: { eq: "assets/icons/whats-app.png" }) {
+                whatsapp: file(
+                    relativePath: { eq: "assets/icons/whats-app.png" }
+                ) {
                     childImageSharp {
-                        fixed(width: 40) {
-                          ...GatsbyImageSharpFixed
+                        fixed(width: 20) {
+                            ...GatsbyImageSharpFixed
                         }
                     }
                 }
-            instagram: 
-                file(relativePath: { eq: "assets/icons/instagram.png" }) {
+                instagram: file(
+                    relativePath: { eq: "assets/icons/instagram.png" }
+                ) {
                     childImageSharp {
                         fixed(width: 60) {
-                          ...GatsbyImageSharpFixed
+                            ...GatsbyImageSharpFixed
                         }
                     }
                 }
-        }
-      `
-    );
+            }
+        `
+    )
     const styleIcon = {
-        margin: '0.75rem'
-    };
+        margin: "0.75rem",
+    }
+    let contactData = []
+    contactData.push(ContactDataPBerg)
+    contactData.push(ContactDataXBerg)
     return (
-    <footer className="footer__wrapper">
-        <address className="footer__container">
-            <div className="footer__address-partition">
-                <p className="footer__address-item">{ContactData.companyName}</p>
-                <p className="footer__address-item">{ContactData.name}</p>
-                <div><Link className="footer__address-item-link" to={NavigationData.footer[0].url}>{NavigationData.footer[0].text}</Link></div> 
-            </div>
-            <div className="footer__address-partition">
-                <div><a className="footer__address-item-link" href={ContactData.address.url} target="_blank" rel="noopener noreferrer">{ContactData.address.street}</a></div>
-                <div><a className="footer__address-item-link" href={ContactData.address.url} target="_blank" rel="noopener noreferrer">{ContactData.address.zip} {ContactData.address.city}</a></div>
-            </div>
-        </address>
-        <div className="footer__container">
+        <footer className="footer__wrapper">
+            {contactData.map(contact => {
+                return (
+                    <React.Fragment key={contact.studioName}>
+                        <div className="footer__studio-wrapper">
+                            <div className="footer__address-partition">
+                                <p className="footer__address-item-title">
+                                    {contact.studioName}
+                                </p>
+                            </div>
+                            <div className="footer__address-partition">
+                                <p className="footer__address-item">
+                                    {contact.contactName}
+                                </p>
+                                <div>
+                                    <img
+                                        className="footer__address-icon-phone"
+                                        src={PhoneIcon}
+                                        alt="call"
+                                    />
+                                    <img
+                                        className="footer__address-icon-phone"
+                                        src={SmsIcon}
+                                        alt="text"
+                                    />
+                                    <a
+                                        className="footer__address-item-link"
+                                        href={contact.tel1.url}
+                                    >
+                                        {contact.tel1.number}
+                                    </a>
+                                    <div className="footer__address-item-container">
+                                        <a
+                                            href={contact.whatsapp.url}
+                                            className="footer__address-item-link"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <Img
+                                                style={{
+                                                    marginRight: "0.25rem",
+                                                }}
+                                                fixed={
+                                                    data.whatsapp
+                                                        .childImageSharp.fixed
+                                                }
+                                                alt="WhatsApp"
+                                            />
+                                        </a>
+                                        <a
+                                            href={contact.whatsapp.url}
+                                            className="footer__address-item-link"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            WhatsApp
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <address className="footer__container-address">
+                                <div className="footer__address-partition">
+                                    <div>
+                                        <a
+                                            className="footer__address-item-link"
+                                            href={contact.address.url}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            {contact.address.street}
+                                        </a>
+                                    </div>
+                                    <div>
+                                        <span className="footer__address-item">
+                                            {contact.address.street2}
+                                        </span>
+                                    </div>
+                                    <div>
+                                        <a
+                                            className="footer__address-item-link"
+                                            href={contact.address.url}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            {contact.address.zip}{" "}
+                                            {contact.address.city}
+                                        </a>
+                                    </div>
+                                </div>
+                            </address>
+                            {contact.socialMedia && (
+                                <div className="footer__container">
+                                    <div>
+                                        {contact.socialMedia.facebook && (
+                                            <a
+                                                href={
+                                                    contact.socialMedia.facebook
+                                                }
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
+                                                <Img
+                                                    style={styleIcon}
+                                                    fixed={
+                                                        data.facebook
+                                                            .childImageSharp
+                                                            .fixed
+                                                    }
+                                                    alt="Facebook"
+                                                />
+                                            </a>
+                                        )}
+                                        {contact.socialMedia.instagram && (
+                                            <a
+                                                href={
+                                                    contact.socialMedia
+                                                        .instagram
+                                                }
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
+                                                <Img
+                                                    fixed={
+                                                        data.instagram
+                                                            .childImageSharp
+                                                            .fixed
+                                                    }
+                                                    alt="Instagram"
+                                                />
+                                            </a>
+                                        )}
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </React.Fragment>
+                )
+            })}
             <div className="footer__address-partition">
                 <div>
-                    <img className="footer__address-icon-phone" src={PhoneIcon} alt="call"/> 
-                    <img className="footer__address-icon-phone" src={SmsIcon} alt="text"/>
-                    <a className="footer__address-item-link" href={ContactData.tel1.url}>{ContactData.tel1.number}</a>
+                    <Link
+                        className="footer__address-item-link"
+                        to={NavigationData.footer[0].url}
+                    >
+                        {NavigationData.footer[0].text}
+                    </Link>
                 </div>
-                <div>
-                    <img className="footer__address-icon-phone" src={PhoneIcon} alt="call"/> 
-                    <img className="footer__address-icon-phone" src={SmsIcon} alt="text"/>
-                    <a className="footer__address-item-link" href={ContactData.tel2.url}>{ContactData.tel2.number}</a>
-                </div>
-                
             </div>
-        </div>
-        <div className="footer__container">
-            <div className="footer__address-partition">
-                <a href={ContactData.socialMedia.facebook} target="_blank" rel="noopener noreferrer">
-                    <Img style={styleIcon} fixed={data.facebook.childImageSharp.fixed} alt="Facebook" />
-                </a>
-
-                <a href={ContactData.socialMedia.whatsapp} target="_blank" rel="noopener noreferrer">
-                    <Img style={styleIcon} fixed={data.whatsapp.childImageSharp.fixed} alt="WhatsApp" />
-                </a>
-                <a href={ContactData.socialMedia.instagram} target="_blank" rel="noopener noreferrer">
-                    <Img fixed={data.instagram.childImageSharp.fixed} alt="Instagram" />
-                </a>
-            </div>
-        </div>
-    </footer>
-)}
+        </footer>
+    )
+}
--- src/pages/studios.js
+++ src/pages/studios.js
@@ -22,7 +22,6 @@ export default ({data}) => {
                 <a className="studio__address-item" href={ContactData.address.url} target="_blank" rel="noopener noreferrer"><p>{ContactData.address.street}</p></a>
                 <a className="studio__address-item" href={ContactData.address.url} target="_blank" rel="noopener noreferrer"><p>{ContactData.address.zip} {ContactData.address.city}</p></a>
                 <a className="studio__address-item" href={ContactData.tel1.url}><p>{ContactData.tel1.number}</p></a>
-                <a className="studio__address-item" href={ContactData.tel2.url}><p>{ContactData.tel2.number}</p></a>
             </div> 
             <div className="studio__map">
                 <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2426.7484859330143!2d13.419246717295579!3d52.5379846596298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e0123a140b5%3A0x249d1cbdc20bfd80!2sSredzkistra%C3%9Fe+59%2C+10405+Berlin!5e0!3m2!1sen!2sde!4v1559236774966!5m2!1sen!2sde" width="600" height="450" frameBorder="0"></iframe>
