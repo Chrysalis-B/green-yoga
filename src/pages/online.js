c4c6df113e2908d7df7a2198428631e666096357
safouman <safouman@gmail.com>
2020-10-29 15:41:54 +0100

    Adding redirect /online to fitogram

---
+++ src/pages/online.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react"
+import { navigate } from "gatsby"
+
+const Online = () => {
+    useEffect(() =>
+        navigate("https://widget.fitogram.pro/green-yoga/?w=/studio")
+    )
+    return <div></div>
+}
+
+export default Online
